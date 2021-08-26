e20a88b59ea339f93b6487972bb8e1a819479b4a
Matías Gutierrez <gutierrez.matias@outlook.com>
2021-08-26 15:50:43 -0300

    Clase 2.2 webapi (#2)
    * adding endpoints of WeatherForecast
    
    * removing comments
    
    * adding ignore and remove comments
    
    * ignore vs folder and remove ignored files
    
    Co-authored-by: Juan Abal <juaniabal7@gmail.com>

--- ObligatorioDA2.Application/WeatherForecasts/ForecastService.cs
+++ ObligatorioDA2.Application/WeatherForecasts/ForecastService.cs
@@ -19,18 +19,33 @@ namespace ObligatorioDA2.Application.WeatherForecasts
         {
             var forecasts = new List<WeatherForecast>();
 
-            if (!_context.WeatherForecasts.Any())
-            {
-                forecasts.AddRange(ForecastFactory.NewWeek());
-                _context.WeatherForecasts.AddRange(forecasts);
-                _context.SaveChanges();
-            }
-            else
+            if (_context.WeatherForecasts.Any())
             {
                 forecasts.AddRange(_context.WeatherForecasts);
             }
-
             return forecasts.Select(Mapper.ToDto);
+        }
+        public WeatherForecastOutputDto Get(int id)
+        {
+
+            return Mapper.ToDto(_context.WeatherForecasts.FirstOrDefault(w => w.Id == id));
+        }
+
+        public void Create(WeatherForecastInputDto forecast)
+        {
+            _context.Add(Mapper.ToModel(forecast));
+            _context.SaveChanges();
+        }
+
+        public void Update(WeatherForecastInputDto forecast)
+        {
+            _context.Update(Mapper.ToModel(forecast));
+            _context.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            _context.Remove(_context.WeatherForecasts.FirstOrDefault(w => w.Id == id));
+            _context.SaveChanges();
         }
     }
 }
--- ObligatorioDA2.HttpApi/Controllers/WeatherForecastController.cs
+++ ObligatorioDA2.HttpApi/Controllers/WeatherForecastController.cs
@@ -22,5 +22,55 @@ namespace ObligatorioDA2.HttpApi.Controllers
         {
             return _forecastService.GetAll();
         }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var forecast = _forecastService.Get(id);
+            if (forecast == null)
+            {
+                return NotFound();
+            }
+            return Ok(forecast);
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] WeatherForecastInputDto forecast)
+        {
+            _forecastService.Create(forecast);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Put([FromBody] WeatherForecastInputDto forecast)
+        {
+            _forecastService.Update(forecast);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Put(int forecastId)
+        {
+            _forecastService.Delete(forecastId);
+            return Ok();
+        }
     }
 }
+/*
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var user = users.Get(id);
+            if (user == null) {
+                return NotFound();
+            }
+            return Ok(UserModel.ToModel(user));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]UserModel model)
+        {
+            try {
+                var user = users.Create(UserModel.ToEntity(model));
+                return CreatedAtRoute("Get", new { id = user.Id }, UserModel.ToModel(user));
+            } catch(ArgumentException e) {
+                return BadRequest(e.Message);
+            }
+        }
+*/
