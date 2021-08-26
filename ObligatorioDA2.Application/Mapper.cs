4134c8406875e2fe5c68456949ea782a50bc8f7d
Juan Abal <juaniabal7@gmail.com>
2021-08-26 13:26:54 -0300

    adding endpoints of WeatherForecast

--- ObligatorioDA2.Application/Mapper.cs
+++ ObligatorioDA2.Application/Mapper.cs
@@ -17,5 +17,16 @@ namespace ObligatorioDA2.Application
                 UserId = forecast.UserId
             };
         }
+        public static WeatherForecast ToModel(WeatherForecastInputDto forecast)
+        {
+            return new WeatherForecast
+            {
+                Id = forecast.Id,
+                Date = forecast.Date,
+                Summary = forecast.Summary,
+                TemperatureC = forecast.TemperatureC,
+                UserId = forecast.UserId
+            };
+        }
     }
 }
---
+++ ObligatorioDA2.Application/WeatherForecasts/Dtos/WeatherForecastInputDto.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ObligatorioDA2.Application.WeatherForecasts.Dtos
+{
+    public class WeatherForecastInputDto
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
