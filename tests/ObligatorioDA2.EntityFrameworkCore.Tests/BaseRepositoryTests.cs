b47a455c577513609249b2f15d38a6887b6b7f17
Matías Gutierrez <gutierrez.matias@outlook.com>
2021-09-21 14:58:33 -0300

    Clase 4.2 mocking (#8)
    * adding endpoints of WeatherForecast
    
    * removing comments
    
    * adding ignore and remove comments
    
    * ignore vs folder and remove ignored files
    
    * dependency injection
    - configure lazy loading
    - refactor webapi
    
    * fix format
    
    * repositories and guards
    - add new package for application contracts
    - add repositories
    - add guards
    
    * fix format
    
    * invert dependencies in ef
    
    * fix merge
    
    * fix format
    
    * add queryparams and new method in repo
    
    * fix format
    
    * test examples of httpapi and efcore projects
    
    * change expected for actual
    
    Co-authored-by: Juan Abal <juaniabal7@gmail.com>

---
+++ tests/ObligatorioDA2.EntityFrameworkCore.Tests/BaseRepositoryTests.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatorioDA2.Domain;
+using ObligatorioDA2.EntityFrameworkCore.Contracts;
+
+namespace ObligatorioDA2.EntityFrameworkCore.Tests
+{
+    [TestClass]
+    public class BaseRepositoryTests
+    {
+        private readonly IRepository<WeatherForecast> _fakeRepo;
+
+        public BaseRepositoryTests()
+        {
+            Context context = ContextFactory.GetInMemoryContext("InMemoryTestDb");
+            context.WeatherForecasts.Add(new WeatherForecast
+            {
+                Summary = "hot",
+                TemperatureC = 30
+            });
+            context.WeatherForecasts.Add(new WeatherForecast
+            {
+                Summary = "cold",
+                TemperatureC = 12
+            });
+            context.SaveChanges();
+            _fakeRepo = new FakeRepository(context);
+        }
+
+        [TestMethod]
+        public void ReadAllWhere_IsSuccessful()
+        {
+            IEnumerable<WeatherForecast> hotForecasts = _fakeRepo.ReadAllWhere(f => f.Summary == "hot");
+            Assert.AreEqual(1, hotForecasts.Count());
+        }
+    }
+}
